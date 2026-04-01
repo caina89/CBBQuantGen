369b7dab4e73c5b3cb85cef232d09d7a586f55a8
Na Cai <caina89@gmail.com>
2026-04-01 10:29:24 +0200

    Update simulate_pheno.R

--- Lecture6Exercise/simulate_pheno.R
+++ Lecture6Exercise/simulate_pheno.R
@@ -1,31 +1,32 @@
 args <- commandArgs(trailingOnly = TRUE)
-bim_file <- args[1]     # 1kg_eur.bim
+bim_pvar_file <- args[1]
 n_causal <- as.numeric(args[2])
 output_prefix <- args[3]
-h2 <- 0.5
 
-# Load the BIM file
-bim <- read.table(bim_file, header = FALSE)
-colnames(bim) <- c("CHR", "SNP", "CM", "BP", "A1", "A2")
+# Read the variant file (handles .bim or .pvar)
+# PLINK2 pvar files might have a header starting with #
+is_pvar <- grepl(".pvar$", bim_pvar_file)
+if(is_pvar){
+  vars <- read.table(bim_pvar_file, comment.char = "#")
+  colnames(vars)[1:5] <- c("CHROM", "POS", "ID", "REF", "ALT")
+} else {
+  vars <- read.table(bim_pvar_file)
+  colnames(vars)[1:6] <- c("CHR", "SNP", "CM", "BP", "A1", "A2")
+  colnames(vars)[2] <- "ID" # Standardize column name
+}
 
-# 1. Randomly sample causal SNPs from the BIM
-# We sample from the whole BIM to ensure they exist
-set.seed(42) # For reproducibility
-causal_indices <- sample(1:nrow(bim), n_causal)
-causal_snps <- bim[causal_indices, ]
+# 1. Randomly sample causal SNPs
+set.seed(42)
+# Filter out variants with missing IDs if necessary
+vars <- vars[vars$ID != ".", ]
+causal_indices <- sample(1:nrow(vars), n_causal)
+causal_vars <- vars[causal_indices, ]
 
-# 2. Assign effect sizes
-# Column 1: SNP ID, Column 2: Effect Allele (A1), Column 3: Beta
+# 2. Create the effects file (SNP ID, then Beta)
 effects <- data.frame(
-    SNP = causal_snps$SNP,
-    A1 = causal_snps$A1,
+    ID = causal_vars$ID,
     Beta = rnorm(n_causal, mean = 0, sd = 1)
 )
 
-# Write the effects file for PLINK --score
 write.table(effects, paste0(output_prefix, ".effects"), 
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
-
-# Save the list of causal SNPs for record-keeping
-write.table(causal_snps$SNP, paste0(output_prefix, ".causal_list"), 
-            quote = FALSE, row.names = FALSE, col.names = FALSE)
